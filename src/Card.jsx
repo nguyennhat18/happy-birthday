2334ce0f76a25c06d9954ec3b01c7f31bc861740
nguyennhat18 <nhatlak551@gmail.com>
2023-04-20 21:12:15 +0700

    upload file to github

---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">HAPPY BIRTHDAY!</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">Chúc Mừng Sinh Nhật Mẹ!</h3>
+      <p>Gửi tới mẹ của con,</p>
+      <p>
+      Con chẳng biết nói gì hơn, nhân ngày sinh nhật mẹ, con chỉ biết chúc mẹ luôn luôn mạnh khoẻ, hạnh phúc và luôn luôn là người mẹ mà con yêu quý nhất. ❤️❤️❤️
+      </p>
+      <p className="name">Nhất</p>
+    </div>
+  </div>
+);
